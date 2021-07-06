6a083cd90cefab94e7db9d6220e69b2c83261acd
Catherine Upperman <catherineupperman@Catherines-MacBook-Air.local>
2021-07-06 17:53:54 -0400

    time blocks and local storage

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,39 +1,47 @@
 //Psudo: GOAL: daily planner to create a schedule
 //TODO: the current day is displayed at the top of the calendar, with running time.
-//Use moment to generate current date and time.
+//IDEALLY: How I want this to work- take the currentTime value and compare it to a specific set of time (like an hour and all the misc min and seconds within that hour)
 var timeDisplayEl = $('#time-display');
 var timeBlkIndex = 0;
 var past = document.querySelector(".past");
 var present = document.querySelector(".present");
 var future = document.querySelector(".future");
 
+//TO DO: click into a timeblock to enter an event, with click save to local storage
+//JSON elements for local storage, click events to save to the local storage 
+$(".saveBtn").on("click", function(){
+    var activity = $(this).siblings(".description").val().trim()
+    //var hour = $(this).siblings(".hour").text().trim()
+    var hour = $(this).parent().attr("id").split("-")[1]
+    localStorage.setItem(hour, activity)
+})
 
+//TODO: timeblocks for standard business hours 8-5 
+var momenthour= moment().hours()
+$(".time-block").each(function(){
+    var ourHour = $(this).attr("id").split("-")[1]
+    console.log(ourHour)
+    
+});
+//Use moment to generate current date and time.
 function currentTime() {
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     timeDisplayEl.text(rightNow);
 }
 
 setInterval(currentTime, 1000);
-// scroll down  (scroll bar needed?)
-//TODO: timeblocks for standard business hours 8-5 
 
-//IDEALLY: How I want this to work- take the currentTime value and compare it to a specific set of time (like an hour and all the misc min and seconds within that hour)
-
-//PONDERING: Can I take a time set using moment like range it from 08:00:00- 8:59:59?
-
-
-//PONDERING: JQuery to build text boxes with a standard save button or would an array with appendages be more efficient??
 var scheduleTime = $('.form-group');
 
-function timeLoop() {
-    if (scheduleTime >= currentTime) {
-        document.querySelector(".present");
-    } if (scheduleTime < currentTime) {
-        document.querySelector(".future")
-    } else {
-        document.querySelector(".past")
-    }
-}
+//function timeLoop() {
+   // if (scheduleTime >= currentTime) {
+        //document.querySelector(".present");
+   // } if (scheduleTime < currentTime) {
+        //document.querySelector(".future")
+   // } else {
+      //  document.querySelector(".past")
+    //}
+//}
 
 
 
@@ -41,6 +49,8 @@ function timeLoop() {
 
 //set three classes past(gray) present(red) future(green)
 //create a function with the variable of the current time, compare to the time set on the buttons to express different colors
+
+//array? of a time loop?
 
 //possible function idea:
 
@@ -57,11 +67,7 @@ function timeLoop() {
 
 
 
-//TO DO: click into a timeblock to enter an event, with click save to local storage
-//JSON elements for local storage, click events to save to the local storage 
-//PONDERING: onclick or maybe eventListener... on click is more inclusive to apply to each button as opposed to just one?
-var schContent = "";
-var scheduleSave = JSON.parse(localStorage.getItem("schContent",)) || [];
+
 
 
 
