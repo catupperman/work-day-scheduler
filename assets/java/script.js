8e0321e6103067de980c81118cd96255765e88c7
Catherine Upperman <catherineupperman@Catherines-MacBook-Air.local>
2021-07-06 22:28:03 -0400

    attempting to make color changes

--- assets/java/script.js
+++ assets/java/script.js
@@ -9,7 +9,7 @@ var future = document.querySelector(".future");
 
 //TO DO: click into a timeblock to enter an event, with click save to local storage
 //JSON elements for local storage, click events to save to the local storage 
-$(".saveBtn").on("click", function(){
+$(".saveBtn").on("click", function () {
     var activity = $(this).siblings(".description").val().trim()
     //var hour = $(this).siblings(".hour").text().trim()
     var hour = $(this).parent().attr("id").split("-")[1]
@@ -17,11 +17,17 @@ $(".saveBtn").on("click", function(){
 })
 
 //TODO: timeblocks for standard business hours 8-5 
-var momenthour= moment().hours()
-$(".time-block").each(function(){
+var momenthour = moment().hours()
+$(".time-block").each(function () {
     var ourHour = $(this).attr("id").split("-")[1]
-    console.log(ourHour)
-    
+    if (currentTime >= ourHour) {
+        $(this).attr(".time-block").past
+    } else if (currentTime <= ourHour) {
+        $(this).attr(".time-block").future
+    } else {
+        $(this).attr(".time-block").present
+    }
+
 });
 //Use moment to generate current date and time.
 function currentTime() {
@@ -31,46 +37,12 @@ function currentTime() {
 
 setInterval(currentTime, 1000);
 
-var scheduleTime = $('.form-group');
-
-//function timeLoop() {
-   // if (scheduleTime >= currentTime) {
-        //document.querySelector(".present");
-   // } if (scheduleTime < currentTime) {
-        //document.querySelector(".future")
-   // } else {
-      //  document.querySelector(".past")
-    //}
-//}
-
 
 
 // TODO: each timeblock is color coded to indicate whether it is in the past, present, or future
 
 //set three classes past(gray) present(red) future(green)
 //create a function with the variable of the current time, compare to the time set on the buttons to express different colors
-
-//array? of a time loop?
-
-//possible function idea:
-
-// function colorCodes (){
-//if (curentTime >= timeBlock[timeBlkIndex]) 
-//display: gray in text box;
-//} if (currentTime <= timeBlock[timeBlkIndex]){
-//display: red in text box;
-//} else{
-//display: green in text box;
-//}
-//return;
-//colorCodes;
-
-
-
-
-
-
-
 
 
 //TODO: WHEN refreshing the page, the saved events persist
