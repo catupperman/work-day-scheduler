03ac90d725f31d56e1508a7c0e99d2f5805d9c7c
Catherine Upperman <catherineupperman@Catherines-MacBook-Air.local>
2021-07-08 22:21:31 -0400

    adding colors

--- assets/java/script.js
+++ assets/java/script.js
@@ -2,10 +2,15 @@
 //TODO: the current day is displayed at the top of the calendar, with running time.
 //IDEALLY: How I want this to work- take the currentTime value and compare it to a specific set of time (like an hour and all the misc min and seconds within that hour)
 var timeDisplayEl = $('#time-display');
-var timeBlkIndex = 0;
-var past = document.querySelector(".past");
-var present = document.querySelector(".present");
-var future = document.querySelector(".future");
+
+
+//Use moment to generate current date and time.
+function currentTime() {
+    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+}
+
+setInterval(currentTime, 1000);
 
 //TO DO: click into a timeblock to enter an event, with click save to local storage
 //JSON elements for local storage, click events to save to the local storage 
@@ -22,27 +27,17 @@ $(".saveBtn").on("click", function () {
 //create a function with the variable of the current time, compare to the time set on the buttons to express different colors
 var momenthour = moment().hours()
 //console.log(momenthour)
-
+//addClass
 $(".time-block").each(function () {
-    var ourHour = $(this).attr("id").split("-")[1]
-    for(i=0; i < 0; i++) {
-        if (momenthour > ourHour){
-        $(".col-10").attr(".past")
-    } else if (momenthour === ourHour){
-        $(".col-10").attr(".present")
+    var chartHour = $(this).attr("id").split("-")[1]
+
+    if (momenthour == chartHour) {
+        $(this).addClass("present")
+    } else if (momenthour < chartHour) {
+        $(this).addClass("past")
     } else {
-        $(".col-10").attr(".future")
+        $(this).addClass("future")
     }
-    }
-    return;
 });
-//Use moment to generate current date and time.
-function currentTime() {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-    timeDisplayEl.text(rightNow);
-}
-
-setInterval(currentTime, 1000);
-
 
 //TODO: WHEN refreshing the page, the saved events persist
